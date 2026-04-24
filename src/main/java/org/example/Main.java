cd5eccc5e05565e0c7e48f17d2fb6a5c0121a593
DevChristPhantom <atamarichristiandev@gmail.com>
2026-04-24 14:50:37 -0500

    Mision 2: Implementacion de logica condicional para descansos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args){
-
+/*
         //declaramos las variables
         String names= "Christian Wilber";
         String lastname="Atamari Hancco";
@@ -17,7 +17,33 @@ public class Main {
 
         System.out.println("Mi NOMBRE ES: " + names + " " + lastname + "\n" +
                 "MI Edad: " + age + "\n" +
+
                 "Mis Horas de Estudio: " + hoursOfStudy);
+  */
+        //codigo para calcular el tiempo de desanso recomendado
+        double liftedWeight= 80.0;
+        int repetitions= 10;
+        int series=4;
+
+       if(repetitions<=0){
+           System.out.println("NO HICISTE NI UNA REPETICION");
+       }
+       else if(repetitions>=1 && repetitions<=5) {
+           System.out.printf("Descanzo 3 minutos");
+
+        }
+       else if(repetitions>=6 && repetitions<=12) {
+           System.out.printf("Descanzo 1 a 2 minutos por la hipertrofia");
+
+       }else{
+           System.out.println("Tu quieres ganar resistencia muscular, descanza 45 segundos");
+       }
+        System.out.println(" ");
+        System.out.println("========DATA OF USERS=========");
+        System.out.println("My liftedWeight is :"+liftedWeight);
+        System.out.println("My repetitions :"+ repetitions);
+
     }
+
 }
 
