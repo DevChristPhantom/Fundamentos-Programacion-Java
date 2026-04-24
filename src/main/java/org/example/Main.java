71b848834c9a494f445a1cc2c3dc8a3015fa7d7e
DevChristPhantom <atamarichristiandev@gmail.com>
2026-04-24 14:15:59 -0500

    Misión 1 completada: Corrección de entorno y primer código Java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args){
+
+        //declaramos las variables
+        String names= "Christian Wilber";
+        String lastname="Atamari Hancco";
+        int age = 24;
+        double hoursOfStudy=4.0 ;
+
+
+        System.out.println("=============== Hello and welcome to MY DATA ===============");
+
+
+        System.out.println("Mi NOMBRE ES: " + names + " " + lastname + "\n" +
+                "MI Edad: " + age + "\n" +
+                "Mis Horas de Estudio: " + hoursOfStudy);
+    }
+}
+
